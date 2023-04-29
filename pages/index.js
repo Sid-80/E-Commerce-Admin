542b9bd2a290d75b465bdd64e2325c3acbac142c
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2023-04-29 12:21:10 +0530

    session added

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.css';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-        <div className='flex bg-violet-300 h-screen w-screen items-center'>
-          <div className='text-center w-full'>
-            <button className='p-2 px-4 rounded-md bg-white'>Login with google</button>
-          </div>
+  const { data: session } = useSession();
+
+  if(!session){
+    return(
+      <div className='flex bg-violet-300 h-screen w-screen items-center'>
+        <div className='text-center w-full'>
+          <button onClick={()=>signIn('google')} className='p-2 px-4 rounded-md bg-white'>Login with google</button>
         </div>
+      </div>
+    )
+  }
+
+
+  return (
+    <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
   )
 }
