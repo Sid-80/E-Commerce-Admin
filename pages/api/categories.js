89afdc073d0fabfac1579c1e46582d709ebd6ef9
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2023-05-05 23:46:29 +0530

    category update and delete done

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -18,4 +18,21 @@ export default async function handler(req,res){
     {
         res.json(await Category.find().populate('parent'));
     }
+    else if(method === 'PUT')
+    {
+        const {name,parentCategory,_id} = req.body;
+        if (parentCategory === 0) {
+            const categoryDoc = await Category.updateOne({_id},{name});
+            res.json({status:true,categoryDoc});
+        }else{
+            const categoryDoc = await Category.updateOne({_id},{name, parent:parentCategory});
+            res.json({status:true,categoryDoc});
+        }
+    }
+    else if(method === 'DELETE')
+    {
+        if(req.query?.id)
+            await Category.deleteOne({_id:req.query?.id});
+        res.json({status:true});
+    }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -7,14 +7,21 @@ import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
 
 export default function categories() {
+    const [editedCategory,setEditedCategory] = useState(null);
     const [name ,setName] = useState('');
     const [categories,setCategories] = useState([]);
     const [parentCategory, setSelectedParent] = useState("");
-    console.log(parentCategory);
+    const [deleteSection,setDeleteSection] = useState(false);
+    const [itemToBeDeleted,setItemDeleted] = useState(null);
     const saveCategory = async(e) => {
         e.preventDefault();
-        
-        const res = await axios.post('/api/categories',{name,parentCategory});
+        var res;
+        if(editCategory){
+            res = await axios.put('/api/categories',{name,parentCategory,_id:editedCategory._id});
+            setEditedCategory(null);
+        }else{
+            res = await axios.post('/api/categories',{name,parentCategory});
+        }
         setName('');
         console.log(res.data)
         if(res.data.status){
@@ -31,14 +38,49 @@ export default function categories() {
     const fetchCategories = () => {
         axios.get('/api/categories').then((res)=>setCategories(res.data));
     }
+    const editCategory = (cat) => {
+        setEditedCategory(cat);
+        setName(cat.name);
+        if(cat.parent !== undefined)
+            setSelectedParent(cat.parent?._id);
+        else
+            setSelectedParent(0);
+    }
+    const changeClass = () => {
+        var delBox = document.getElementById('deleteBox');
+
+        setDeleteSection(!deleteSection);
+        if (deleteSection) {
+            document.getElementById('delTitle').innerHTML = `Do you want to delete ${itemToBeDeleted.name} ?`;
+            document.getElementById('backDel').classList.add('showBack')
+            delBox.classList.add('displayBox');
+        }else{
+            delBox.classList.remove('displayBox');
+            document.getElementById('backDel').classList.remove('showBack')
+        }
+    }
+    const deleteCategory = (cat) => {
+        setItemDeleted(cat);
+        changeClass();
+    }
+    const deleteCategoryYes = async() => {
+        const res = await axios.delete(`/api/categories?id=${itemToBeDeleted?._id}`);
+        if(res.data.status){
+            toast.error(`${itemToBeDeleted?.name} deleted`,options);
+        }
+        changeClass();
+        fetchCategories();
+        setItemDeleted(null);
+    }
     useEffect(()=>{
         fetchCategories();
+        setDeleteSection(false);
     },[])
   return (
     <Layout>
-         <div className='flex flex-col h-[100%] w-[100%] overflow-hidden justify-start items-start'>
+         <div className='flex relative z-0 flex-col h-[100%] w-[100%] overflow-hidden justify-start items-start'>
             <div className='flex flex-col w-[50%]'>
-            <h1 className='font-bold tracking-[5px] animate-pulse my-8'>CATEGORIES</h1>
+            <h1 className='font-bold tracking-[5px] animate-pulse my-8'>{editedCategory ? `EDIT CATEGORY ${editCategory.name}` : `NEW CATEGORY`}</h1>
             <form className='w-full p-5 flex gap-1' onSubmit={(e)=>saveCategory(e)}>
                 
                 <div>
@@ -79,16 +121,28 @@ export default function categories() {
                                     }
                                 </div>
                                 <div className='flex gap-4 px-4'>
-                                    <Link href={'/'}>
+                                    <button onClick={()=>editCategory(cat)}>
                                         <PencilSquareIcon className='h-8 w-8 hover:text-green-300' />
-                                    </Link>
-                                    <Link href={'/'}>
+                                    </button>
+                                    <button onClick={()=>deleteCategory(cat)}>
                                         <TrashIcon className='h-8 w-8 hover:text-red-600' />
-                                    </Link>
+                                    </button>
                                 </div>
                             </div>
                         ))
                     }
+                </div>
+            </div>
+            <div id='backDel' className=' absolute opacity-0 w-full hidden h-full backdrop-filter backdrop-blur-xl '>
+                <div
+                id='deleteBox'
+                className={`-z-10 absolute flex flex-col items-center justify-center rounded-lg shadow-2xl left-[40%] w-[400px] h-[150px] bg-white top-[45%]`}
+                >
+                    <h1 id='delTitle' className='font-bold text-xl'></h1>
+                    <div className='flex gap-2 py-2'>
+                        <button onClick={()=>deleteCategoryYes()} className='p-1 hover:ring px-3 bg-[#F2D7D9] text-lg rounded-md text-black'>YES</button>
+                        <button onClick={changeClass} className='p-1 px-3 hover:ring bg-[#F2D7D9] text-lg rounded-md text-black'>NO</button>
+                    </div>
                 </div>
             </div>
         </div>
